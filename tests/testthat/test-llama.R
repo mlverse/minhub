5a865d12086c4aceaa4ff0e0ff76e01da327538b
Daniel Falbel <dfalbel@gmail.com>
2023-08-30 15:01:22 -0300

    Codellama and llama2 (#2)
    * Code llama is actually just llama with a few different parameters and decisions around what's included in the state dict.
    
    * Added support for running official codellama
    
    * remove specialized files
    
    * Update readme.
    
    * Re-document for additional arguments.
    
    * augment tolerance

--- tests/testthat/test-llama.R
+++ tests/testthat/test-llama.R
@@ -12,3 +12,20 @@ test_that("Can create a llama model", {
   reference <- c(0.2226,  0.0299,  0.2729, -0.7919,  1.6164)
   expect_equal(as.numeric(out), reference, tolerance = 1e-4)
 })
+
+test_that("can create predictions", {
+  skip_on_ci()
+
+  model <- llama_from_pretrained("codellama/CodeLlama-7b-hf")
+  model$to(dtype=torch_float32())
+  model$eval()
+  with_no_grad({
+    pred <- model(torch_ones(1, 3, dtype="long") + 1L)
+  })
+
+  out <- pred[1, 1, 1:5]
+
+  reference <- c(-7.1554, -13.0050,   5.4070,  -6.0711,  -5.8435)
+  expect_equal(as.numeric(out), reference, tolerance = 1e-5)
+
+})
